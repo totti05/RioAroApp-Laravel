a857b9668db131bf361bb1d4f1fdcf7c182e2ce2
totti05 <raul_leandro_0504@hotmail.com>
2019-12-13 19:58:16 +0000

    se añadieron los campos correspondientes al formulario de registro de persona, sin validacion

--- resources/views/person/register.blade.php
+++ resources/views/person/register.blade.php
@@ -5,7 +5,9 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                <div class="card-header">{{ __('registrar informacion personal') }}</div>
+                    <div class="card-header">
+                        {{ __('registrar informacion personal') }}
+                    </div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('person.store') }}">
@@ -25,41 +27,99 @@
                                 </div>
                             </div>
 
-                        <div class="form-group row">
-                            <label for="apellido" class="col-md-4 col-form-label text-md-right">{{ __('apellido') }}</label>
+                            <div class="form-group row">
+                                <label for="apellido" class="col-md-4 col-form-label text-md-right">{{ __('apellido') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="apellido" type="text" class="form-control @error('apellido') is-invalid @enderror" name="apellido">
+                                <div class="col-md-6">
+                                    <input id="apellido" type="text" class="form-control @error('apellido') is-invalid @enderror" name="apellido">
 
-                                @error('apellido')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                    @error('apellido')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                                
+                            <div class="form-group row">
+                                <label for="cedula" class="col-md-4 col-form-label text-md-right">{{ __('cedula') }}</label>
+                                
+                                <div class="col-md-6">
+                                    <input id="cedula" type="text" class="form-control @error('cedula') is-invalid @enderror" name="cedula">
+
+                                    @error('cedula')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="urbanizacion" class="col-md-4 col-form-label text-md-right">{{ __('urbanizacion') }}</label>
+                                
+                                <div class="col-md-6">
+                                    <input id="urbanizacion" type="text" class="form-control @error('urbanizacion') is-invalid @enderror" name="urbanizacion">
+
+                                    @error('urbanizacion')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
                             
-                        <div class="form-group row">
-                            <label for="cedula" class="col-md-4 col-form-label text-md-right">{{ __('cedula') }}</label>
-                            
-                            <div class="col-md-6">
-                                <input id="cedula" type="text" class="form-control @error('cedula') is-invalid @enderror" name="cedula">
+                            <div class="form-group row">
+                                <label for="manzana" class="col-md-4 col-form-label text-md-right">{{ __('manzana') }}</label>
+                                
+                                <div class="col-md-6">
+                                    <input id="manzana" type="text" class="form-control @error('manzana') is-invalid @enderror" name="manzana">
 
-                                @error('cedula')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                    @error('manzana')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('next') }}
-                                </button>
+                            <div class="form-group row">
+                                <label for="casa" class="col-md-4 col-form-label text-md-right">{{ __('casa') }}</label>
+                                
+                                <div class="col-md-6">
+                                    <input id="casa" type="text" class="form-control @error('casa') is-invalid @enderror" name="casa">
+
+                                    @error('casa')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
-                    </form>
+
+                            <div class="form-group row">
+                                <label for="fecha_nacimiento" class="col-md-4 col-form-label text-md-right">{{ __('fecha de nacimiento') }}</label>
+                                
+                                <div class="col-md-6">
+                                    <input id="fecha_nacimiento" type="text" class="form-control @error('fecha_nacimiento') is-invalid @enderror" name="fecha_nacimiento">
+
+                                    @error('fecha_nacimiento')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4 ">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('next') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
