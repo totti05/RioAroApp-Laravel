ca7fd8250787c463a27499eaa63bdc252aa4e238
totti05 <raul_leandro_0504@hotmail.com>
2019-12-20 05:33:34 +0000

    se creo el seed y el factory para los activities, funciona correctamente

---
+++ database/factories/ActivityFactories.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Activity;
+use Faker\Generator as Faker;
+
+$factory->define(Activity::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence(),
+        'description' => $faker->text(100)
+    ];
+});
