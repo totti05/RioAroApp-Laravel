7a4f3ef8c1c700b31a1276787c14f62f78d04188
totti05 <raul_leandro_0504@hotmail.com>
2019-12-23 16:05:38 +0000

    se modifico el factory y el seeder para person, ya funciona correctamente

--- database/factories/PersonFactory.php
+++ database/factories/PersonFactory.php
@@ -5,14 +5,15 @@
 use App\Person;
 use Faker\Generator as Faker;
 
+
 $factory->define(Person::class, function (Faker $faker) {
     return [
-        'dni' => $faker->numerify('V########')->unique(),
-        'name' => $faker->name,
+        'dni' => $faker->numerify('V########'),
+        'name' => $faker->firstname,
         'lastname' => $faker->lastname,
         'urbanization' =>$faker->city,
         'street' => $faker->randomDigit() ,
-        'house_numer' => bothify('#?'),
-        'birthdate' => date($format = 'Y-m-d', $max = 'now') // '1979-06-09'
+        'house_number' => $faker->bothify('#?'),
+        'birthdate' => $faker->date($format = 'Y-m-d', $max = 'now') // '1979-06-09'
     ];
 });
--- database/seeds/PersonTableSeeder.php
+++ database/seeds/PersonTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Person;
 
 class PersonTableSeeder extends Seeder
 {
@@ -11,6 +12,19 @@ class PersonTableSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(PersonTableSeeder::class);
+        DB::table('People')->insert([
+            'id' => '1',
+            'dni' => 'V22820863',
+            'name' => 'Raul',
+            'lastname' => 'Leandro',
+            'urbanization' => 'Rio Aro',
+            'street' => '16',
+            'house_number' => '24',
+            'birthdate' => '1994-04-05',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        
+        factory(Person::class,10)->create();
     }
 }
